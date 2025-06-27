687e0228b0905f719e279581ec869fceb08534f7
Dakalo Ndonde <phildasdakalo@gmail.com>
2025-06-27 03:58:57 +0200

    Create brute-force.py

---
+++ LEARN PYTHON/brute-force.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+print("""
+--------------------------------
+    Web App Brute Force Tool    
+--------------------------------
+""")
+
+targetURL = input("Enter URL: ")
+print("Target URL:",targetURL)
